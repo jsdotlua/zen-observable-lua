45f438ceb20133ff44eb6c13bbfd63047a7b2ba4
jeparlefrancais <jeparlefrancais21@gmail.com>
2023-12-21 18:11:15 -0500

    fix build single file script

--- scripts/build-single-file.sh
+++ scripts/build-single-file.sh
@@ -5,7 +5,8 @@ set -e
 mkdir -p build
 mkdir -p build/debug
 
-rm -f build/luau-requests.lua
+rm -f build/zen-observable.lua
+rm -f build/debug/zen-observable.lua
 
 darklua process --config .darklua-bundle.json src/init.lua build/zen-observable.lua
 darklua process --config .darklua-bundle-dev.json src/init.lua build/debug/zen-observable.lua
